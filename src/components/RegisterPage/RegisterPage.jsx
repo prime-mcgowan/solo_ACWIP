33d28b5e95c4504a4336a893a82672906dbdee99
Laura McGowan <prime.lmcg@gmail.com>
2023-02-09 15:49:22 -0600

    volunteerPortalPage rough out of to-do tracker and contactCorner done...all inputs working

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -18,7 +18,7 @@ function RegisterPage() {
             history.push('/login');
           }}
         >
-          Login
+          {/* Login */}
         </button>
       </center>
     </div>
--- src/components/VolunteerPortalPage/VolunteerPortalPage.jsx
+++ src/components/VolunteerPortalPage/VolunteerPortalPage.jsx
@@ -7,6 +7,12 @@ import React from 'react';
 function VolunteerPortalPage() {
 
     const [toDo, setToDo] = useState('');
+    const [to, setTo] = useState('');
+    const [from, setFrom] = useState('');
+    const [date, setDate] = useState('');
+    const [subject, setSubject] = useState('');
+    const [letterContent, setLetterContent] = useState('');
+
 
 
 
@@ -18,18 +24,33 @@ function VolunteerPortalPage() {
 
         dispatch({
             type: 'SAGA/CREATE_NEW_TO-DO', 
-            payload: newToDo,
+            payload: toDo,
         })
+        setToDo('');
     }//end handleToDoSumbission
 
 
     const handleNewLetterSubmission = (event) => {
         event.preventDefault();
 
+        let newLetter = {
+            to: to, 
+            from: from, 
+            subject: subject,
+            date: date,
+            letterContent: letterContent
+        }
+
+        console.log('This is a new letter', newLetter);
+
         dispatch({
             type: 'SAGA/CREATE_NEW_LETTER',
             payload: newLetter
         })
+        setTo('');
+        setFrom('');
+        setSubject('');
+        setLetterContent('');
     }
 
 
@@ -45,7 +66,10 @@ function VolunteerPortalPage() {
 
                 <div className="row g-3">
                     <div className="col-sm-9">
-                        <input type="text" className="form-control" placeholder="My To-Do" aria-label="City"/>
+                        <input type="text" className="form-control" placeholder="My To-Do" aria-label="City" value={toDo} onChange={(event) => setToDo(event.target.value)}/>
+                    </div>
+                    <div className="col-sm">
+                        <button className="btn btn-primary profile-button" type="button" onClick={handleToDoSubmission}>ADD</button>
                     </div>
                     <div className="col-sm">
                         <button className="btn btn-primary profile-button" type="button">DONE</button>
@@ -65,16 +89,18 @@ function VolunteerPortalPage() {
                     </div>
             
                 <div className="mb-3">
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="To"/>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="From"/>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Subject"/>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="To" value={to} onChange={(event) => setTo(event.target.value)}/>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="From" value={from} onChange={(event) => setFrom(event.target.value)}/>
+                    <input type="date" className="form-control" id="exampleFormControlInput1" placeholder="From" value={date} onChange={(event) => setDate(event.target.value)}/>
+
+                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Subject" value={subject} onChange={(event) => setSubject(event.target.value)}/>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={letterContent} onChange={(event) => setLetterContent(event.target.value)}></textarea>
                 </div>
 
-                <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="button">SEND</button></div>
+                <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="button" onClick={handleNewLetterSubmission} >SEND</button></div>
 
             </div>
-            {/* end Contact Corner Containger */}
+            {/* end Contact Corner Container */}
 
 
 
