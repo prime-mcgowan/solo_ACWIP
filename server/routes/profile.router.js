03ca1dc228e982bd7c9f8e7b300f1aee96695fa2
Laura McGowan <prime.lmcg@gmail.com>
2023-02-12 13:46:30 -0600

    ALL editVolunteerForm inputs are working correctly and loading to the database

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -98,10 +98,28 @@ router.put('/', (req, res) => {
   UPDATE volunteer_profiles
       SET
         first_name=$1,
-      WHERE 
-        id=$2
+        last_name=$2,
+        email=$3,
+        birthday=$4,
+        address=$5,
+        apt_suite_number=$6,
+        city=$7,
+        state=$8,
+        zip_code=$9,
+        is_monday_mailers=$10,
+        is_penpal_program=$11,
+        is_operation_holidays=$12,
+        is_get_on_the_bus=$13,
+        bio_response=$14
+        
+        
+      WHERE id=$15
   `;
-  pool.query(sqlQuery, [req.body.first_name, idToUpdate])
+  pool.query(sqlQuery, [req.body.first_name,req.body.last_name, req.body.email,
+            req.body.birthday,req.body.address, req.body.apt_suite_number, req.body.city,
+            req.body.state, req.body.zip_code, req.body.is_monday_mailers, 
+            req.body.is_penpal_program, req.body.is_operation_holidays,
+            req.body.is_get_on_the_bus, req.body.bio_response, idToUpdate])
     .then((result) => {
         res.sendStatus(200);
     })
@@ -111,54 +129,3 @@ router.put('/', (req, res) => {
     });
 })
 module.exports = router;
-
-
- // "last_name"=$2
-        // "email"
-        // "birthday"
-        // "address"
-        // "apt_suite_number",
-        // "city"
-        // "state"
-        // "zip_code"
-        // "is_monday_mailers"
-        // "is_penpal_program"
-        // "is_operation_holidays",
-        // "is_get_on_the_bus"
-        // "bio_response"
-        // "user_id"
-
-
-
-        // email=$3,
-        // birthday=$4, 
-        // address=$5,
-        // apt_suite_number=$6,
-        // city=$7,
-        // state=$8,
-        // zip_code=$9,
-
-
-        // is_monday_mailers=$10,
-        // is_penpal_program=$11,
-        // is_opearation_holidays=$12, 
-        // is_get_on_the_bus=$13,
-
-        // req.body.email,
-        // req.body.birthday, 
-        // req.body.address,
-        // req.body.apt_suite_number,
-        // req.body.city,
-        // req.body.state,
-        // req.body.zip_code, 
-        
-
-
-
-
-
-
-        // req.body.is_monday_mailers,
-      //   req.body.is_penpal_program,
-      // req.body.is_operation_holidays, 
-      // req.body.is_get_on_the_bus, 
